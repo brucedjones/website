c430d0dbe2940d7de1d0f7afeb85e9e532de5da5
Bruce Jones <bruce.david.jones@gmail.com>
2016-04-27 20:05:24 -0400

    Test

--- src/drbdjones.js
+++ src/drbdjones.js
@@ -48,6 +48,7 @@ app.get('/', function(req, res) {
     res.render('home',social);
 });
 
+//Required for heroku
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
