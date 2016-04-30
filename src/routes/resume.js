9b060666cce831a690d45524070b403a232e1ab0
Bruce Jones <bruce.david.jones@gmail.com>
2016-04-30 18:28:28 -0400

    Resume working
    Also improved landing page

--- src/routes/resume.js
+++ src/routes/resume.js
@@ -2,7 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/resume', function(req, res) {
-	res.render('resume');
+	res.locals.db.collection("skills").find({}).toArray(function(err, docs) {
+    	if (err) {
+    	  	res.status(500).send({error:"Failed to get data from database"});
+    	} else {
+    		res.locals.skills = docs;
+
+    		res.locals.db.collection("work").find({}).toArray(function(err, docs) {
+    			if (err) {
+    	  			res.status(500).send({error:"Failed to get data from database"});
+    			} else {
+    				res.locals.work = docs;
+
+					res.locals.db.collection("education").find({}).toArray(function(err, docs) {
+    					if (err) {
+    	  					res.status(500).send({error:"Failed to get data from database"});
+    					} else {
+    						res.locals.education = docs;
+
+							res.locals.db.collection("activities").find({}).toArray(function(err, docs) {
+    							if (err) {
+    	  							res.status(500).send({error:"Failed to get data from database"});
+    							} else {
+    								res.locals.activities = docs;
+									res.render('resume');
+    							}
+  							});
+    					}
+  					});
+    			}
+  			});
+    	}
+  	});
 });
 
 module.exports = router;
