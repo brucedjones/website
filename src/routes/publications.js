9c0742bb211b015c5a1faa64339fb95e89f4a98f
Bruce Jones <bruce.david.jones@gmail.com>
2016-05-01 21:30:06 -0400

    Finished publications

--- src/routes/publications.js
+++ src/routes/publications.js
@@ -2,19 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/publications', function(req, res) {
+
     res.locals.db.collection("journal").find({}).toArray(function(err, docs) {
         if (err) {
             res.status(500).send({error:"Failed to get data from database"});
         } else {
+            docs = docs.sort(function(a,b){
+                return a.year<b.year;
+            });
             res.locals.journal = docs;
-            //res.locals.db.collection("conference").find({}).toArray(function(err, docs) {
-            //    if (err) {
-            //        res.status(500).send({error:"Failed to get data from database"});
-            //    } else {
-            //        res.locals.conference = docs;
+
+            res.locals.db.collection("conference").find({}).toArray(function(err, docs) {
+                if (err) {
+                    res.status(500).send({error:"Failed to get data from database"});
+                } else {
+                    docs = docs.sort(function(a,b){
+                        return a.year<b.year;
+                    });
+                    
+                    res.locals.conference = docs;
                     res.render('publications');
-            //    }
-            //});
+                }
+            });
         }
     });
 });
