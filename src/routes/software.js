4e30874a9a4940445fbf91faad0eb2b5f293a1fd
Bruce Jones <bruce.david.jones@gmail.com>
2016-05-04 20:58:55 -0400

    Test design for software
    Panels won't go on the same row...

--- src/routes/software.js
+++ src/routes/software.js
@@ -1,32 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-var GitHubApi = require("github");
-
-var github = new GitHubApi({
-    // required 
-    version: "3.0.0",
-    // optional 
-    debug: true,
-    protocol: "https",
-    host: "api.github.com", // should be api.github.com for GitHub
-    timeout: 5000,
-    headers: {
-        "user-agent": "dr-bdjones" // GitHub is happy with a unique user agent 
-    }
-});
-
 router.get('/software', function(req, res) {
 
     res.locals.fixed_footer = true;
 
-    github.repos.get({
-    	user: "brucedjones",
-    	repo: "website"
-	}, function(err, docs) {
-		res.locals.software = JSON.stringify(docs);
-		res.render('software');
-	});
+    res.locals.software = [];
+    res.locals.software.push({title:"This Website",
+    	github:"website",
+    	description:"This site served two purposes. <ul> <li>Finally set up a proper website for myself.</li> <li>A good oppurtunity to become more familiar with Express and Heroku.</li></ul>",
+    	tags:["Node.js","Express","Heroku"]
+    });
+
+    res.locals.software.push({title:"This Website",
+    	github:"website",
+    	description:"This site served two purposes. <ul> <li>Finally set up a proper website for myself.</li> <li>A good oppurtunity to become more familiar with Express and Heroku.</li></ul>",
+    	tags:["Node.js","Express","Heroku"]
+    });
+
+    res.locals.software.push({title:"This Website",
+    	github:"website",
+    	description:"This site served two purposes. <ul> <li>Finally set up a proper website for myself.</li> <li>A good oppurtunity to become more familiar with Express and Heroku.</li></ul>",
+    	tags:["Node.js","Express","Heroku"]
+    });
+
+    res.locals.software.push({title:"This Website",
+    	github:"website",
+    	description:"This site served two purposes. <ul> <li>Finally set up a proper website for myself.</li> <li>A good oppurtunity to become more familiar with Express and Heroku.</li></ul>",
+    	tags:["Node.js","Express","Heroku"]
+    });
+
+    res.render('software');
     
 });
 
