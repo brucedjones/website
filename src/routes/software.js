21ceb5a50d3479a1c6daf71d45b037f0e2fada0e
Bruce Jones <bruce.david.jones@gmail.com>
2016-05-29 11:38:48 -0400

    Software now using ttlData

--- src/routes/software.js
+++ src/routes/software.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var ttlData = require('../ttlData');
+var software = new ttlData(20000,5);
+
 router.get('/software', function(req, res) {
 
     res.locals.fixed_footer = false;
 
-    res.locals.db.collection("software").find({}).toArray(function(err, docs) {
-        if (err) {
-            var error = {code:"500",description:"<p>Something went wrong! Please try again in a few minutes.</p><p>If the problem persists please contact contact <a href='mailto:bdjones@mit.edu'>bdjones@mit.edu</a></p>"};
-			res.locals.error = error;
-			res.locals.fixed_footer = true;
-			res.status(500).render('error');
-        } else {
-            res.locals.software = docs;
-            res.render('software');
-        }
-    });
+    var finalize = function(data){
+		res.locals.software = data;
+        res.render('software');
+	};
+
+	var loadData = function(callback){
+		res.locals.db.collection("software").find({}).toArray(function(err, docs) {
+    		if (err) {
+    		  	callback();
+    		} else {
+    			callback(docs);
+    		}
+  		});
+	};
+
+	var error = function(callback){
+		var error = {code:"500",description:"<p>Something went wrong! Please try again in a few minutes.</p><p>If the problem persists please contact contact <a href='mailto:bdjones@mit.edu'>bdjones@mit.edu</a></p>"};
+		res.locals.error = error;
+		res.locals.fixed_footer = true;
+		res.status(500).render('error');
+	};
+
+	software.doCached(loadData, finalize, error);
     
 });
 
